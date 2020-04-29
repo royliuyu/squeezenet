52148d97a62087a1a76ea3a320371f6efa921528
royliuyu <47387577+royliuyu@users.noreply.github.com>
2020-04-29 01:53:52 -0400

    main code with conv1 and fire2
    main code with conv1 and fire2

--- src/main.c
+++ src/main.c
@@ -18,10 +18,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "main.h"
 #include "convol.h"
 #include "defines.h"
 #include "utility.h"
 #include "relu.h"
+#include "padding.h"
+#include "MAXpool.h"
 
 
 //**********************************************************************
@@ -33,107 +36,74 @@
 // Return: 0 if success                                                *
 //**********************************************************************
 int main(int argc, char** argv) {
-
-	int imageWidth, imageHeight, kernelSize,convWidth, convHeight, padding, stride;
-	int imageCount;
-	float* kernel;
-	float* convOutput;
-	float* reluOutput;
-	unsigned char* image;
-
-	int kernalSize_fire2Sq; //kerel size of fire2 squeeze, 1 * 1
-	float* filterFire2Sq; // 16 kernals
-	float* fire2SqIn;
-	float* fire2SqOut;
-	int convWidth_fire2Sq;
-	int convHeight_fire2Sq;
-
-	int kernalSize_fire2Expd1; //kerel size of fire2 squeeze, 1 * 1
-	float* filterFire2Expd1; // 16 kernals
-	float* fire2Expd1In;
-	float* fire2Expd1Out;
-	int convWidth_fire2Expd1;
-	int convHeight_fire2Expd1;
-
-	int kernalSize_fire2Expd3; //kerel size of fire2 squeeze, 3 * 3
-	float* filterFire2Expd3; // 16 kernals
-	float* fire2Expd3In;
-	float* fire2Expd3Out;
-	int convWidth_fire2Expd3;
-	int convHeight_fire2Expd3;
-
-
+	
 	imageCount = 0;
-	imageWidth = IMAGE_WIDTH;
-	imageHeight = IMAGE_HEIGHT;
 
-	while (imageCount<10) {
-	char filename[50];//filename length upto 50
-	sprintf(filename, "images/viptraffic%d.ppm", imageCount);//read viptrafficX.ppm
-	image = (unsigned char*)malloc(imageHeight * imageWidth * sizeof(unsigned char) * 3);
-	readPPM(image, filename); //get image data from file
+//	read 160*120 ppm pictures , total 120 frames
+//	while (imageCount<10) {
+//	char filename[50];//filename length upto 50
+//	sprintf(filename, "images/viptraffic%d.ppm", imageCount);//read viptrafficX.ppm
+//	image = (unsigned char*)malloc(imageHeight * imageWidth * sizeof(unsigned char) * 3);
+//	readPPM(image, filename); //get image data from file
+	
+	//read image
+	imgWidth = IMAGE_WIDTH ; //image is color's with RGB
+	imgHeight = IMAGE_HEIGHT;
+	image = (unsigned char*)malloc(imgHeight * imgWidth * 3 * sizeof(unsigned char));
+	readPPM(image, "uncc.ppm");
 
+	//image processing, add padding =3 for processing, RGB
+	imgLayer = (unsigned char*)malloc((imgHeight+3)* (imgWidth+3) * 3 * sizeof(unsigned char));
+	PadImage(image,imgLayer, imgWidth, imgHeight, 3); //transfer size from 224x224 to 227x227 pad a image , add totally 3 pad, RGB 
+	imgLayerWidth = imgWidth+3;
+	imgLayerHeight = imgHeight+3;
 
+	//convolution 1, 96 kernals, kernel size 7, padding 0, stride 2
+	conv1In= imgLayer;
+	conv1Width = (imgLayerWidth - 7 +0)/2+1; //kernel 7*7, padding 0, stride 2
+	conv1Height = (imgLayerHeight - 7 +0)/2+1; 
+	conv1Out =	(unsigned char*)malloc(3 * conv1Width * conv1Height *sizeof(unsigned char));
+	conv(conv1In,conv1Out,fltConv1,imgLayerWidth,imgLayerHeight, 96, 7, 0, 2); 
+	
+	//max pool, pool1 : pool one, filter 3, padding 0, stride 2
+	p1In = conv1Out;
+	p1Width = (conv1Width - 3 + 0)/2 +1;
+	p1Width = (conv1Width - 3 + 0)/2 +1;
+	p1Out = (unsigned char*)malloc(3 * p1Width * p1Height * sizeof(unsigned char)); //
+	MAXpool(p1In, p1Out, 3, 2, 0);
 
-	imageWidth = IMAGE_WIDTH * 3; //image is color's with RGB
-	imageHeight = IMAGE_HEIGHT * 3;
-	padding = 0;
-	stride =1;
-	kernelSize =3;
-	convWidth = (imageWidth - kernelSize + 2 * padding) / stride + 1;
-	convHeight = (imageHeight - kernelSize + 2 * padding) / stride + 1;  
+	//fire2 squeeze , 16 kernels, kernel size 1, padding 0, stride 1
+	f2SqWidth = p1Width; //1x1 convolution
+	f2SqHeight = p1Height;
+	f2SqIn= p1Out;
+	f2SqOut = (unsigned char*)malloc(3 * f2SqWidth * f2SqHeight * sizeof(unsigned char));
+	fltF2Sq = (float*)malloc(3  *  1 * 1 * 16 * sizeof(float));
+	randomInit(fltF2Sq, (3 * 1 * 1 * 16), 255);
+	conv(f2SqIn, f2SqOut,fltF2Sq, f2SqWidth, f2SqHeight,16,1,0,1); 
+	relu(f2SqOut,(3 * f2SqWidth * f2SqHeight)); //relu data size = size of fire2_sqOut
 
+	//fire2 expand 1 x 1:  64 kernels, kernel size 1, padding 0, stride 1
+	f2Ex1Width = f2SqWidth; //1x1 convolution
+	f2Ex1Height = f2SqHeight;
+	f2Ex1In = f2SqOut;
+	f2Ex1Out = (unsigned char*)malloc(3 * f2Ex1Width * f2Ex1Height * sizeof(unsigned char));
+	fltF2Ex1 = (float*)malloc(3  *  1 * 1 * 64 * sizeof(float));
+	randomInit(fltF2Ex1, (3 * 1 * 1 * 64), 255);
+	conv(f2Ex1In, f2Ex1Out,fltF2Ex1, f2Ex1Width, f2Ex1Height,64,1,0,1); 
 
-	/*convolution 1
-	...
-	*/
-
-	/* max pools 1
-		...
-	*/
-
-	//fire2 squeeze , filter:  3*  1*1*16
-
-	convWidth_fire2Sq= imageWidth * 16;
-	convHeight_fire2Sq= imageWidth * 16;
-	kernalSize_fire2Sq = 1 *1;
-	filterFire2Sq = (float*)malloc(3  *  1 * 1 * 16 * sizeof(float));
-	fire2SqIn = (float *) image; // connect to previous layer's output
-	fire2SqOut = (float*)malloc(convWidth_fire2Sq * convHeight_fire2Sq * sizeof(float));
-	randomInit(filterFire2Sq, (kernalSize_fire2Sq*16), 255); //initialize kernel, 16 kernels
-	convolute(fire2SqIn,  filterFire2Sq, fire2SqOut, convWidth_fire2Sq, convHeight_fire2Sq,16,1,0,1); // kernal number 16, kernel size 1, padding 0, stride 1
-	relu(fire2SqOut,(convWidth_fire2Sq * convHeight_fire2Sq)); //relu data size = size of fire2_sqOut
-
-	//fire2 expand 1 x 1, filter: 3*  1*1*64
-
-
-	convWidth_fire2Expd1= convWidth_fire2Sq * 64; // 64 channels for 64 number of kernel
-	convHeight_fire2Expd1= convHeight_fire2Sq * 64;
-	kernalSize_fire2Expd1 = 1*1;
-	filterFire2Expd1 = (float*)malloc(3  *  1 * 1 * 64 * sizeof(float));
-	fire2Expd1In = fire2SqOut; // connect to the output of fire2 squeeze
-	fire2Expd1Out = (float*)malloc(convWidth_fire2Expd1 * convHeight_fire2Expd1 * sizeof(float)); //filter is 64 of kernel
-	randomInit(filterFire2Expd1, (kernalSize_fire2Expd1*64), 255); //initialize kernel, 16 kernels
-	convolute(fire2Expd1In,  filterFire2Expd1, fire2Expd1Out, convWidth_fire2Expd1, convHeight_fire2Expd1,64,1,0,1); // kernal number 64, kernel size 1, padding 0, stride 1
-	relu(fire2Expd1Out,(convWidth_fire2Expd1 * convHeight_fire2Expd1 )); 
-
-	//fire2 expand 3 x 3, filter: 3*  1*1*64
-
-
-	convWidth_fire2Expd3= convWidth_fire2Expd1 * 64; // 64 channels for 64 number of kernel
-	convHeight_fire2Expd3= convHeight_fire2Expd1 * 64;
-	kernalSize_fire2Expd3 = 3*3;
-	filterFire2Expd3 = (float*)malloc(3  *  3 * 3 * 64 * sizeof(float));
-	fire2Expd3In = fire2Expd1Out; // connect to the output of fire2 expand 1X1 layer
-	fire2Expd1Out = (float*)malloc(convWidth_fire2Expd3 * convHeight_fire2Expd3 * sizeof(float)); //filter is 64 of kernel
-	randomInit(filterFire2Expd3, (kernalSize_fire2Expd3*64), 255); //initialize kernel, 64 kernels
-	convolute(fire2Expd3In,  filterFire2Expd3, fire2Expd3Out, convWidth_fire2Expd3, convHeight_fire2Expd3,64,1,0,1); // kernal number 64, kernel size 1, padding 0, stride 1
-	relu(fire2Expd3Out,(convWidth_fire2Expd3 * convHeight_fire2Expd3 )); 
-
+	//fire2 expand 3 x 3:  64 kernels, kernel size 3, padding 0, stride 1
+	f2Ex3Width = f2Ex1Width + 3 +1; 
+	f2Ex3Height = f2Ex1Height + 3 +1;
+	f2Ex3In = f2SqOut;
+	f2Ex3Out = (unsigned char*)malloc(3 * f2Ex3Width * f2Ex3Height * sizeof(unsigned char));
+	fltF2Ex3 = (float*)malloc(3  *  3 * 3 * 64 * sizeof(float));
+	randomInit(fltF2Ex1, (3 * 3 * 3 * 64), 255);
+	conv(f2Ex3In, f2Ex3Out,fltF2Ex3, f2Ex3Width, f2Ex3Height,64,3,0,1); 
+	relu(f2SqOut,(3 * f2Ex3Width * f2Ex3Height)); 
 
 	imageCount ++;
 
-	} // end of 120 images processing
+//	} // end of while, which process 120 images 
 	
 	// Output..........
 
@@ -141,13 +111,14 @@ int main(int argc, char** argv) {
 
 	//clear memory
 
-
-	free (filterFire2Sq);
-	free (fire2SqOut);
-	free (filterFire2Expd1);
-	free (fire2Expd1Out);
-	free (filterFire2Expd3);
-	free (fire2Expd3Out);
+	//free(image);
+	//free(imageLayer);
+	//free (filterFire2Sq);
+	//free (fire2SqOut);
+	//free (filterFire2Expd1);
+	//free (fire2Expd1Out);
+	//free (filterFire2Expd3);
+	//free (fire2Expd3Out);
 
 	return 0;
 }
