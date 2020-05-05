0c976846130fd8f6e9f8c302f5f29d3ae1e61038
royliuyu <47387577+royliuyu@users.noreply.github.com>
2020-05-05 12:37:03 -0400

    updated 5/5/2020
    finish debugging for conv1 to fire2

--- src/convol.h
+++ src/convol.h
@@ -2,12 +2,12 @@
 #define __CONVOL_H__
 
 int ConvRGB(unsigned char* convInput, int* convOutput, float *kernel, \
-			  int imageW, int imageH, int dimension, int channel, int kernelSize, int padding, int stride) ; // RGB convolution, only support dimension = 1 
+			  int imageW, int imageH, int channel, int dimension, int kernelSize, int padding, int stride) ; // RGB convolution, only support dimension = 1 
 
 int Conv(int* convIn, int* convOut, float *knl, \
-			  int imgW, int imgH, int dim, int chn, int knlSize, int pd, int strd) ;
+			  int imgW, int imgH, int chn, int dim, int knlSize, int pd, int strd) ;
 
 int Conv1x1(int* convIn1, int* convOut1, float *knl1, \
-			   int imgW1, int imgH1, int dim1, int chn1, int knlSize1, int pd1, int strd1) ;
+			   int imgW1, int imgH1, int chn1, int dim1, int knlSize1, int pd1, int strd1);
 
 #endif
--- src/main.h
+++ src/main.h
@@ -11,26 +11,34 @@
 	unsigned char* conv1In;
 	int* conv1Out;
 	int conv1Width, conv1Height;
+	unsigned char* conv1OutChar;
 
 	//maxPool 1
 	int* p1In;
 	int* p1Out; //max Pool one
 	int p1Width, p1Height;
+	unsigned char* p1OutChar;
 
 	//fire 2
 	int* f2SqIn;
 	int* f2SqOut;
 	int f2SqWidth, f2SqHeight;
 	float* fltF2Sq;
+	unsigned char* f2SqOutChar;
 
 	int f2Ex1Width,f2Ex1Height;
 	int* f2Ex1In;
 	int* f2Ex1Out;
 	float * fltF2Ex1;
+	unsigned char* f2Ex1OutChar;
 
 	int f2Ex3Width,f2Ex3Height;
 	int* f2Ex3In;
 	int* f2Ex3Out;
 	float * fltF2Ex3;
+	unsigned char* f2Ex3OutChar;
 
 	int* f2Out;
+	unsigned char* f2OutChar;
+
+	int i;
